ff1a4ce64add3095527c44a309cd3216bf539302
Jesús Jurado Rodríguez <jesusjuro2005@gmail.com>
2025-04-29 10:54:00 +0200

    fix #6 corrección datos.py

--- datos.py
+++ datos.py
@@ -1,6 +1,15 @@
 import json
 
 def cargar_datos(ruta):
+    """
+    Carga datos desde un archivo JSON dado su ruta.
+
+    Parámetros:
+    ruta (str): Ruta al archivo JSON.
+
+    Retorna:
+    dict: Datos cargados desde el archivo.
+    """
     archivo = open(ruta)
     datos = json.load(archivo)
     archivo.close()
