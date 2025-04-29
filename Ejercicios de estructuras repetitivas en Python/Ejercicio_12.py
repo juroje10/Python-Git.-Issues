fe59154c3f48c78ac886fb695122c0dead2a9923
Jesús Jurado Rodríguez <jesusjuro2005@gmail.com>
2025-04-29 10:22:27 +0200

    Eliminación de archivos

--- Ejercicios de estructuras repetitivas en Python/Ejercicio_12.py
+++
@@ -1,4 +0,0 @@
-# Ejercicio 12: Programa que pide una cadena y dos caracteres. Sustituye el primer carácter por el segundo en toda la cadena.
-# Autor --> Jesús Jurado Rodríguez
-# Fecha --> 26/10/2024
-
--- Ejercicios de estructuras repetitivas en Python/Ejercicio_8.py
+++
@@ -1,4 +0,0 @@
-# Ejercicio 8: Programa que muestra un cronómetro en pantalla, incrementando horas, minutos y segundos cada segundo.
-# Autor --> Jesús Jurado Rodríguez
-# Fecha --> 26/10/2024
-
